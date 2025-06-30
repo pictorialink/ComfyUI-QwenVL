22966685a63011b74f8f44fee638f59df0355e4f
杜宇航 <duyuhang@huanqiu.com>
2025-06-30 19:30:23 +0800

    feat:add qwen3

--- __init__.py
+++ __init__.py
@@ -4,11 +4,15 @@ from .nodes import *
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     "Qwen2.5VL": Qwen2VL,
-    "Qwen2.5": Qwen2
+    "Qwen2.5": Qwen2,
+    "LoadQwen": LoadQwen,
+    "RunQwen": RunQwen
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Qwen2.5VL": "Qwen2.5VL",
     "Qwen2.5": "Qwen2.5",
+    "LoadQwen": "LoadQwen",
+    "RunQwen": "RunQwen"
 }
--- nodes.py
+++ nodes.py
@@ -13,6 +13,9 @@ import numpy as np
 import folder_paths
 import subprocess
 import uuid
+from mlx_lm import load ,generate
+
+
 
 
 def tensor_to_pil(image_tensor, batch_index=0) -> Image:
@@ -230,9 +233,11 @@ class Qwen2:
         self.model_checkpoint = None
         self.tokenizer = None
         self.model = None
-        self.device = (
-            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-        )
+        if torch.backends.mps.is_available():
+            self.device = torch.device("mps")
+        else:
+            self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        
         self.bf16_support = (
             torch.cuda.is_available()
             and torch.cuda.get_device_capability(self.device)[0] >= 8
@@ -337,6 +342,8 @@ class Qwen2:
                 device_map="auto",
                 quantization_config=quantization_config,
             )
+            # 将模型移动到可用设备
+            self.model = self.model.to(self.device)
 
         with torch.no_grad():
             messages = [
@@ -348,7 +355,8 @@ class Qwen2:
                 messages, tokenize=False, add_generation_prompt=True
             )
 
-            inputs = self.tokenizer([text], return_tensors="pt").to("cuda")
+            # inputs = self.tokenizer([text], return_tensors="pt").to("cuda")
+            inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
 
             generated_ids = self.model.generate(**inputs, max_new_tokens=max_new_tokens)
             generated_ids_trimmed = [
@@ -367,7 +375,226 @@ class Qwen2:
                 del self.model
                 self.tokenizer = None
                 self.model = None
-                torch.cuda.empty_cache()
-                torch.cuda.ipc_collect()
+                if torch.cuda.is_available():
+                    torch.cuda.empty_cache()
+                    torch.cuda.ipc_collect()
 
             return result
+
+class LoadQwen:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model": (
+                    [
+                        "Qwen2.5-3B-Instruct",
+                        "Qwen2.5-7B-Instruct",
+                        "Qwen2.5-14B-Instruct",
+                        "Qwen2.5-32B-Instruct",
+                    ],
+                    {"default": "Qwen2.5-7B-Instruct"},
+                ),
+                "quantization": (
+                    ["none", "4bit", "8bit"],
+                    {"default": "none"},
+                ),  # add quantization type selection
+                
+                "seed": ("INT", {"default": -1}),  # add seed parameter, default is -1
+            },
+        }
+
+    RETURN_TYPES = ("MODEL",)
+    RETURN_NAMES = ("MODEL",)
+    FUNCTION = "inference"
+    CATEGORY = "Comfyui_QwenVL"
+
+    def inference(
+        self,
+        model,
+        quantization,
+        seed,
+    ):
+        Q = QwenModel() 
+        if Q.model is not None and Q.tokenizer is not None:
+            print(f"[QwenModel] Using cached model: {Q.model_checkpoint}")
+            return ({"MODEL": Q},)
+        
+        if torch.backends.mps.is_available():
+            print("Using MPS")
+            Q.MpsLoad(model)
+        else:
+            print("Using CUDA")
+            Q.CudaLoad(model,seed,quantization)
+        # 将模型移动到可用设备
+        # self.model = self.model.to(self.device)
+        return ({"MODEL": Q},)
+
+       
+
+class RunQwen:
+    @classmethod
+    def INPUT_TYPES(cls):
+        inputs = {
+            "required": {
+                "config": ("MODEL", ),
+                "system": (
+                    "STRING",
+                    {
+                        "default": "You are a helpful assistant.",
+                        "multiline": True,
+                    },
+                ),
+                "prompt": ("STRING", {"default": "", "multiline": True}),
+                "max_new_tokens": (
+                    "INT",
+                    {"default": 512, "min": 128, "max": 2048, "step": 1},
+                ),
+                "keep_model_loaded": ("BOOLEAN", {"default": False}),
+                "temperature": (
+                    "FLOAT",
+                    {"default": 0.7, "min": 0, "max": 1, "step": 0.1},
+                ),
+            }
+        }
+        return inputs
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("STRING",)
+    FUNCTION = "execute"
+    CATEGORY = "Comfyui_QwenVL"
+    def execute(self, config, system, prompt, max_new_tokens, keep_model_loaded, temperature):
+        model = config["MODEL"].model
+        tokenizer = config["MODEL"].tokenizer
+        device = config["MODEL"].device
+        with torch.no_grad():
+            messages = [
+                {"role": "system", "content": system},
+                {"role": "user", "content": prompt},
+            ]
+
+            if torch.backends.mps.is_available():
+                if tokenizer.chat_template is not None:
+                    text = tokenizer.apply_chat_template(
+                        messages,
+                        tokenize=False,
+                        add_generation_prompt=True,
+                        enable_thinking=False,  # Setting enable_thinking=False disables thinking mode
+                    )
+                response = generate(
+                    model,
+                    tokenizer,
+                    prompt=text,
+                    verbose=False,
+                    max_tokens=2048
+                )
+                del tokenizer
+                del model
+                return (response,)
+            else:
+                text = tokenizer.apply_chat_template(
+                    messages, tokenize=False, add_generation_prompt=True
+                )
+
+                inputs = tokenizer([text], return_tensors="pt").to(device)
+                generated_ids = model.generate(**inputs, max_new_tokens=max_new_tokens)
+                generated_ids_trimmed = [
+                    out_ids[len(in_ids) :]
+                    for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+                ]
+                result = tokenizer.batch_decode(
+                    generated_ids_trimmed,
+                    skip_special_tokens=True,
+                    clean_up_tokenization_spaces=False,
+                    temperature=temperature,
+                )
+                #执行完毕要清除config
+                del tokenizer
+                del model
+                tokenizer = None
+                model = None
+                return result
+    
+ 
+
+class QwenModel:
+    _instance = None
+    _initialized = False
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._initialized = False
+        return cls._instance
+
+    def __init__(self):
+        if self._initialized:
+            return
+        self._initialized = True
+        self.model_checkpoint = None
+        self.tokenizer = None
+        self.model = None
+        if torch.backends.mps.is_available():
+            self.device = torch.device("mps")
+        else:
+            self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        
+        self.bf16_support = (
+            torch.cuda.is_available()
+            and torch.cuda.get_device_capability(self.device)[0] >= 8
+        )
+
+    def MpsLoad(self, model):
+        model_id = f"qwen/{model}"
+        self.model_checkpoint = os.path.join(
+            folder_paths.models_dir, "LLM", os.path.basename(model_id)
+        )
+        if not os.path.exists(self.model_checkpoint):
+            from huggingface_hub import snapshot_download
+            snapshot_download(
+                repo_id=model_id,
+                local_dir=self.model_checkpoint,
+                local_dir_use_symlinks=False,
+            )
+        model, tokenizer = load(self.model_checkpoint)
+        self.tokenizer = tokenizer
+        self.model = model
+
+    def CudaLoad(self, model,seed,quantization):
+        if seed != -1:
+            torch.manual_seed(seed)
+        model_id = f"qwen/{model}"
+        self.model_checkpoint = os.path.join(
+            folder_paths.models_dir, "LLM", os.path.basename(model_id)
+        )
+
+        if not os.path.exists(self.model_checkpoint):
+            from huggingface_hub import snapshot_download
+            snapshot_download(
+                repo_id=model_id,
+                local_dir=self.model_checkpoint,
+                local_dir_use_symlinks=False,
+            )
+
+        if self.tokenizer is None:
+            self.tokenizer = AutoTokenizer.from_pretrained(self.model_checkpoint)
+
+        if self.model is None:
+            # Load the model on the available device(s)
+            if quantization == "4bit":
+                quantization_config = BitsAndBytesConfig(
+                    load_in_4bit=True,
+                )
+            elif quantization == "8bit":
+                quantization_config = BitsAndBytesConfig(
+                    load_in_8bit=True,
+                )
+            else:
+                quantization_config = None
+
+
+            self.model = AutoModelForCausalLM.from_pretrained(
+                self.model_checkpoint,
+                torch_dtype=torch.bfloat16 if self.bf16_support else torch.float16,
+                device_map="auto",
+                quantization_config=quantization_config,
+            )
